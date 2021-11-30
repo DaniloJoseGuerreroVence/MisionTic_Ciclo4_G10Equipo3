885bbfc7b2fcea591ac0c01c5f77db340db4f94d
DaniloGuerrero <dajogere@gmail.com>
2021-11-29 23:02:11 -0500

    Componentes de pie de pagina y boton modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-// import react from 'react';
- import HeaderInfo from './Components/HeaderInfo/';
+import { useState } from 'react';
+import HeaderInfo from './Components/HeaderInfo/';
 // import "./components/HeaderInfo/Header.module.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button,Carousel,Nav,Navbar,Container,NavDropdown,Form,FormControl,Card } from 'react-bootstrap';
+import { Button, Carousel, Nav, Navbar, Container, NavDropdown, Form, FormControl, Card, Modal } from 'react-bootstrap';
 
-import im1 from './b1.jpg';
-import im2 from './b2.jpg';
-import im3 from './b3.jpg';
+// import im1 from '../../public/b1.jpg';
+import im1 from '../src/assets/img/b1.jpg';
+import im2 from '../src/assets/img/b2.jpg';
+import im3 from '../src/assets/img/b3.jpg';
 
 
 // const App = () =>{
@@ -23,108 +24,186 @@ function App() {
   return (
     <div>
       <header>
-        <HeaderInfo msg="Hola mundo props 2 "/>
+        <HeaderInfo estilo="cabezera" msg=" " />
 
 
-<Navbar bg="light" expand="lg">
-  <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
-      </Nav>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Navbar style={{ background: '#0d59af', }} variant="dark" expand="lg">
+          <Container fluid>
+            <Navbar.Brand ><span style={{ fontSize: 50, marginLeft: 185 }}>Badminton Play</span></Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
 
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
 
+                <Nav.Link style={{ marginLeft: 50 }} href="#action1">Home</Nav.Link>
 
+                {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown> */}
 
-<Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={im1}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={im2}
-      alt="Second slide"
-    />
+              </Nav>
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={im3}
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-
-
-<h1>Eventos disponibles para apostar</h1>
-
-
-<Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-light">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
       </header>
+
+
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={im1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={im2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={im3}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+
+      <h1>Eventos disponibles para apostar</h1>
+
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+
+      {/* <Example /> */}
+
+      <div className="row" style={{ background: '#1565c0', color: 'white', height: 300 }}>
+        <div className="col-3">
+          <div className="fs-4 " > About </div>
+          <div> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</div>
+        </div>
+        <div className="col-3">
+          <div className="fs-4 " > My Account </div>
+          <div> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</div>
+        </div>
+        <div className="col-3">
+          <div className="fs-4 " > Information </div>
+          <div> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</div>
+        </div>
+        <div className="col-3">
+          <div className="fs-4 " > Contacts </div>
+          <div> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</div>
+        </div>
+      </div>
+
+
+      {/* <div className="container">
+<div className="row">
+  <div className="col">
+    Column
+  </div>
+  <div className="col">
+    Column
+  </div>
+  <div className="col">
+    Column
+  </div>
+</div>
+</div> */}
+
+<HeaderInfo estilo="pie" msg="© 2016 New Shop . All rights reserved | Design by W3layouts" />
+
     </div>
+
   );
+}
+
+
+function Example() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch static backdrop modal
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+
+  // render(<Example />);
 }
 
 export default App;
--- src/Components/HeaderInfo/HeaderInfo.js
+++ src/Components/HeaderInfo/HeaderInfo.js
@@ -6,17 +6,26 @@ class HeaderInfo extends Component{
         return(
             // <div style={{backgroundColor:'blue'}}>
 
-            <div className="cabezera">
+            <div className={this.props.estilo}>
               {/* <h2>{this.props.msg}</h2> */}
 
-              <div class="top-left">
-                    <a href="#"> Help 018000-123-1234 </a>
-             </div>
-             <div class="top-right">
+              {/* <div class="top-left">
+                    <span> Help 018000-123-1234 </span>
+             </div> */}
+             <div class="top-right">               
+                   
+
                    <ul>                   
                        <li><a href="login.html">Inicia session</a></li>
                        <li><a href="registered.html"> Crea tu cuenta </a></li>
                    </ul>
+                 
+                                  
+                   <ul>                   
+                       <li><a href="login.html">© 2016 New Shop . All rights reserved | Design by W3layouts</a></li>
+                   </ul>                   
+                   
+                
               </div>
 
             </div>
